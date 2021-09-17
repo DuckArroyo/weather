b5920927b4716dff8409a1f8e4517af9193f0f0f
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-17 15:21:18 -0700

    good code

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -4,6 +4,7 @@ var apiKey = "af8d552e7c5f4110dc63b9ba4e35ef63";
 var cityEl = document.querySelector("#city");
 var stateEl = document.querySelector("#state");
 var zipCodeEl = document.querySelector("#zipCode");
+var responseContainerEl = document.querySelector("#response-container");
 
 var searchCityUrl =
   "https://api.openweathermap.org/data/2.5/forecast?q=Portland&appid=af8d552e7c5f4110dc63b9ba4e35ef63";
@@ -25,71 +26,83 @@ var searchZipUrl =
 //id=submit-zip
 
 function getLa() {
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=af8d552e7c5f4110dc63b9ba4e35ef63")
-.then(response.json(response));
-.then(console.log(data));
-if(response.cod === 200) {
-  console.log(city.name.text);
-  //! code the remove class hide element.remove.class("hide");
-  var cityTitleEl = document.querySelector("#card-title");
-  cityTitleEl.innerHTML = '<h5>' + city.name.text + '</h5>';
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=af8d552e7c5f4110dc63b9ba4e35ef63"
+  ) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); //this is where the data is actually in.
+      if (data.cod === "200") {
+        var cityName = data.city.name; //write variables for each item that need to be displayed
+        var cityNameEl = document.createElement("p");
+        cityNameEl.textContent = cityName;
+        responseContainerEl.appendChild(cityNameEl);
+        //loop for data array
+        console.log(data.city.name);
 
-  var temperatureEl = 
-} 
-else {alert(city + " is not a valid city, please search for a valid city 😩");}
+        //! write the display functions within this function.
 
+        // //! code the remove class hide element.remove.class("hide");
+        // var cityTitleEl = document.querySelector("#card-title");
+        // cityTitleEl.innerHTML = '<h5>' + city.name.text + '</h5>';
+
+        // var temperatureEl =
+      } else {
+        alert(city + " is not a valid city, please search for a valid city 😩");
+      }
+    });
 }
 
-function getChi() {
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=af8d552e7c5f4110dc63b9ba4e35ef63");
+// function getChi() {
+// fetch("https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=af8d552e7c5f4110dc63b9ba4e35ef63");
 
-}
+// }
 
-function get Tokyo(){
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=tokyo&appid=af8d552e7c5f4110dc63b9ba4e35ef63");
+// function get Tokyo(){
+// fetch("https://api.openweathermap.org/data/2.5/forecast?q=tokyo&appid=af8d552e7c5f4110dc63b9ba4e35ef63");
 
-}
+// }
 
 //call 5 day By city
-var getWeather = function () {
-  fetch(searchCityUrl).then(function (response) {
-    if (response.ok) {
-      response.json(function (data) {
-        console.log(response, data);
-        printWeather(data, city);
-      });
-    } else {
-      alert(city + " is not a valid city, please search for a valid city 😩");
-    }
-  });
-};
-getWeather();
+// var getWeather = function () {
+//   fetch(searchCityUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json(function (data) {
+//         console.log(response, data);
+//         printWeather(data, city);
+//       });
+//     } else {
+//       alert(city + " is not a valid city, please search for a valid city 😩");
+//     }
+//   });
+// };
+// getWeather();
 
-// call by zip code
-var getByZip = function () {
-  fetch(searchZipUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-var responseContainerEl = document.querySelector("#response-container");
-responseContainerEl.appendChild()
-      printWeather(city);
-    });
+// // call by zip code
+// var getByZip = function () {
+//   fetch(searchZipUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (response) {
+//       var responseContainerEl = document.querySelector("#response-container");
+//       responseContainerEl.appendChild();
+//       printWeather(city);
+//     });
 
-  // if (response) {
-  //   console.log(data);
-  //   printWeather();
-  // } else {
-  //   alert(
-  //     zipCode + " is not a valid zip code, please search for a valid city 😩"
-  //   );
-  // }
-};
-getByZip();
+// if (response) {
+//   console.log(data);
+//   printWeather();
+// } else {
+//   alert(
+//     zipCode + " is not a valid zip code, please search for a valid city 😩"
+//   );
+// }
+// };
+// getByZip();
 
-var printWeather = function () {
-  console.log("printWeather triggered");
-  //! addClass(".hide"); // class to hide element
-};
-//printWeather();
+// var printWeather = function () {
+//   console.log("printWeather triggered");
+//   //! addClass(".hide"); // class to hide element
+// };
+// //printWeather();
