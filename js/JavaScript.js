0ac7407d202d6814e25e2b24fdb52fd5450d6fdd
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-18 01:07:04 -0700

    sleep

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -4,7 +4,6 @@ var apiKey = "af8d552e7c5f4110dc63b9ba4e35ef63";
 var cityEl = document.querySelector("#city");
 var zipCodeEl = document.querySelector("#zipCode");
 var favoriteEl = document.querySelector("#favorite");
-
 var responseContainerEl = document.querySelector("#response-container");
 
 var getLosAngeles =
@@ -27,14 +26,50 @@ var searchZipUrl =
   zipCodeEl +
   "&units=imperial&appid=" +
   apiKey;
-//!may need the +s
+
+//!call 5 day By city
+cityEl.addEventListener("submit-city", function () {
+  //I believe this can be rewritten as getWeather() {
+  var getWeather = function () {
+    fetch(searchCityUrl) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); //this is where the data is actually in.
+        if (data.cod === 200) {
+          // City name
+          var cityName = data.name.city; //write variables for each item that need to be displayed
+          var cityNameEl = document.createElement("h2");
+          cityNameEl.textContent = cityName;
+          responseContainerEl.appendChild(cityNameEl);
+          //loop for data array
+          console.log(data.name.city);
+          //Temperature
+        } else {
+          alert(
+            city + " is not a valid city, please search for a valid city 😩"
+          );
+        }
+      });
+  };
+});
+//getWeather();
+
+//! call by zip code
+// var getByZip = function () {
+//   fetch(searchZipUrl)
+
+// };
+// getByZip();
 
 function getLa() {
+  reset();
   fetch(getLosAngeles) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
       if (data.cod === 200) {
+        // remove hide
+        responseContainerEl.classList.toggle("hide");
         // City name
         var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
@@ -47,16 +82,25 @@ function getLa() {
         var temperatureEl = document.createElement("p");
         temperatureEl.textContent = temperature;
         responseContainerEl.appendChild(temperatureEl);
-        //loop for data array
         console.log(data.main.temp);
-
-        //! write the display functions within this function.
-
-        // //! code the remove class hide element.remove.class("hide");
-        // var cityTitleEl = document.querySelector("#card-title");
-        // cityTitleEl.innerHTML = '<h5>' + city.name.text + '</h5>';
-
-        // var temperatureEl =
+        //find
+        // find(weather, [0], description) return description
+        // console.log(description);
+        //for loop to collect "Weather"
+        //   var main = for (i = 0; i < weather.length; i++) {
+        //   //description
+        //   var weather = data.weather.description; //write variables for each item that need to be displayed
+        //   var weatherEl = document.createElement("p");
+        //   weatherEl.textContent = weather;
+        //   responseContainerEl.appendChild(weatherEl);
+        //   console.log(data.weather.description);
+        //   //icon
+        //   var icon = data.weather.icon;
+        //   var iconEl = document.createElement("div");
+        //   iconEl.img = icon;
+        //   responseContainerEl.appendChild(iconEl);
+        //   console.log(data.weather.icon);
+        // }
       } else {
         alert("Error😩");
       }
@@ -64,89 +108,62 @@ function getLa() {
 }
 
 function getChi() {
+  reset();
   fetch(getChicago)
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
-      if (data.cod === "200") {
-        var cityName = data.city.name; //write variables for each item that need to be displayed
+      if (data.cod === 200) {
+        responseContainerEl.classList.toggle("hide");
+        // City name
+        var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
         responseContainerEl.appendChild(cityNameEl);
-
         //loop for data array
-        console.log(data.city.name);
-
-        //! write the display functions within this function.
-
-        // //! code the remove class hide element.remove.class("hide");
-        // var cityTitleEl = document.querySelector("#card-title");
-        // cityTitleEl.innerHTML = '<h5>' + city.name.text + '</h5>';
-
-        // var temperatureEl =
+        console.log(data.name);
+        //Temperature
+        var temperature = data.main.temp; //write variables for each item that need to be displayed
+        var temperatureEl = document.createElement("p");
+        temperatureEl.textContent = temperature;
+        responseContainerEl.appendChild(temperatureEl);
+        console.log(data.main.temp);
       } else {
-        alert(city + " is not a valid city, please search for a valid city 😩");
+        alert("Error 😩");
       }
     });
 }
 
 function getTokyo() {
+  reset();
   fetch(getTokyoJP)
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
-      if (data.cod === "200") {
-        var cityName = data.city.name; //write variables for each item that need to be displayed
+      if (data.cod === 200) {
+        responseContainerEl.classList.toggle("hide");
+        // City name
+        var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
         responseContainerEl.appendChild(cityNameEl);
-
         //loop for data array
-        console.log(data.city.name);
-
-        //! write the display functions within this function.
-
-        // //! code the remove class hide element.remove.class("hide");
-        // var cityTitleEl = document.querySelector("#card-title");
-        // cityTitleEl.innerHTML = '<h5>' + city.name.text + '</h5>';
-
-        // var temperatureEl =
+        console.log(data.name);
+        //Temperature
+        var temperature = data.main.temp; //write variables for each item that need to be displayed
+        var temperatureEl = document.createElement("p");
+        temperatureEl.textContent = temperature;
+        responseContainerEl.appendChild(temperatureEl);
+        console.log(data.main.temp);
       } else {
-        alert(city + " is not a valid city, please search for a valid city 😩");
+        alert("Error 😩");
       }
     });
 }
 
-//!call 5 day By city
-var getWeather = function () {
-  fetch(searchCityUrl) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); //this is where the data is actually in.
-      if (data.cod === 200) {
-        // City name
-        var cityName = data.name.city; //write variables for each item that need to be displayed
-        var cityNameEl = document.createElement("h2");
-        cityNameEl.textContent = cityName;
-        responseContainerEl.appendChild(cityNameEl);
-        //loop for data array
-        console.log(data.name.city);
-        //Temperature
-      } else {
-        alert(city + " is not a valid city, please search for a valid city 😩");
-      }
-    });
+var reset = function () {
+  responseContainerEl.textContent = "";
 };
-getWeather();
-
-//! call by zip code
-// var getByZip = function () {
-//   fetch(searchZipUrl)
-
-// };
-// getByZip();
-
-var reset = function () {};
 
 // var printWeather = function () {
 //   console.log("printWeather triggered");
