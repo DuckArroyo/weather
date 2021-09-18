e19db5cabe54a14636ab49b41ff0010a734909e3
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-18 16:57:51 -0700

    drawing data from weather array

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -14,85 +14,84 @@ var getChicago =
 var getTokyoJP =
   "https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=imperial&appid=af8d552e7c5f4110dc63b9ba4e35ef63";
 
-var searchCityUrl =
-  "api.openweathermap.org/data/2.5/forecast?q=" +
-  cityEl +
-  "&units=imperial&appid=" +
-  apiKey;
-
-var searchZipUrl =
-  "api.openweathermap.org/data/2.5/forecast?zip=" +
-  zipCodeEl +
-  "&units=imperial&appid=" +
-  apiKey;
-
 //!call 5 day By city
-//cityEl.addEventListener("submit-city", function () {
-//I believe this can be rewritten as getWeather() {
-var getWeather = function () {
+var getWeather = function (event) {
+  event.preventDefault();
+  var userCity = document.querySelector("#city").value;
+  var searchCityUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    userCity +
+    "&units=imperial&appid=" +
+    apiKey;
+  //console.log(searchCityUrl);
   fetch(searchCityUrl) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
-      if (data.cod === 200) {
+      if (data.cod == 200) {
+        responseContainerEl.classList.toggle("hide");
+        var card = document.createElement("div");
+        card.setAttribute("id", "weather-card");
+        card.setAttribute("class", "card");
+        responseContainerEl.appendChild(card);
         // City name
-        var cityName = data.name.city; //write variables for each item that need to be displayed
+        var cityName = data.city.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
-        responseContainerEl.appendChild(cityNameEl);
+        card.appendChild(cityNameEl);
         //loop for data array
-        console.log(data.name.city);
 
-        //! replicate the loop from LA in here for the 5 day forecast/create
-        //within the loop
-        //create a div for each day
-        //take dt and moment it into day of the week and date
-        //temperature
-        //description
-        //icon
-        //append all into the div
+        for (var i = 0; i < 5; i++) {
+          makeCard(data.list[i]);
+        }
       } else {
         alert(city + " is not a valid city, please search for a valid city 😩");
       }
     });
 };
-//});
+
 //getWeather(); //not used becasue it is triggered by the event listener
 
 //! call by zip code - waiting on the code from city to adapt
-var getByZip = function () {
-  fetch(searchZipUrl);
-  console
-    .log("triggered")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+var getByZip = function (event) {
+  event.preventDefault();
+  var userZip = document.querySelector("#zipCode").value;
+  console.log(userZip);
+  var searchZipUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?zip=" +
+    userZip +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  console.log(searchZipUrl);
 };
-// getByZip();//not used becasue it is triggered by the event listener
+// getByZip();//not used because it is triggered by the event listener
 
 //!these work but the reset function needs to be edited
 function getLa() {
-  reset();
   fetch(getLosAngeles) //currently the name of data is what I told the app that the curlies are named {} name can be used multiple times.
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
       if (data.cod === 200) {
-        // remove hide
+        reset();
         responseContainerEl.classList.toggle("hide");
+        var card = document.createElement("div");
+        card.setAttribute("id", "weather-card");
+        card.setAttribute("class", "card");
+        responseContainerEl.appendChild(card);
         // City name
         var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
-        responseContainerEl.appendChild(cityNameEl);
+        card.appendChild(cityNameEl);
         //loop for data array
         console.log(data.name);
         //Temperature
         var temperature = data.main.temp; //write variables for each item that need to be displayed
         var temperatureEl = document.createElement("p");
         temperatureEl.textContent = temperature;
-        responseContainerEl.appendChild(temperatureEl);
+        card.appendChild(temperatureEl);
         console.log(data.main.temp);
         //!HELP - for loop or find function?
         //find
@@ -120,25 +119,29 @@ function getLa() {
 }
 
 function getChi() {
-  reset();
   fetch(getChicago)
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
       if (data.cod === 200) {
+        reset();
         responseContainerEl.classList.toggle("hide");
+        var card = document.createElement("div");
+        card.setAttribute("id", "weather-card");
+        card.setAttribute("class", "card");
+        responseContainerEl.appendChild(card);
         // City name
         var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
-        responseContainerEl.appendChild(cityNameEl);
+        card.appendChild(cityNameEl);
         //loop for data array
         console.log(data.name);
         //Temperature
         var temperature = data.main.temp; //write variables for each item that need to be displayed
         var temperatureEl = document.createElement("p");
         temperatureEl.textContent = temperature;
-        responseContainerEl.appendChild(temperatureEl);
+        card.appendChild(temperatureEl);
         console.log(data.main.temp);
       } else {
         alert("Error 😩");
@@ -147,25 +150,29 @@ function getChi() {
 }
 
 function getTokyo() {
-  reset();
   fetch(getTokyoJP)
     .then((response) => response.json())
     .then((data) => {
       console.log(data); //this is where the data is actually in.
       if (data.cod === 200) {
+        reset();
         responseContainerEl.classList.toggle("hide");
+        var card = document.createElement("div");
+        card.setAttribute("id", "weather-card");
+        card.setAttribute("class", "card");
+        responseContainerEl.appendChild(card);
         // City name
         var cityName = data.name; //write variables for each item that need to be displayed
         var cityNameEl = document.createElement("h2");
         cityNameEl.textContent = cityName;
-        responseContainerEl.appendChild(cityNameEl);
+        card.appendChild(cityNameEl);
         //loop for data array
         console.log(data.name);
         //Temperature
         var temperature = data.main.temp; //write variables for each item that need to be displayed
         var temperatureEl = document.createElement("p");
         temperatureEl.textContent = temperature;
-        responseContainerEl.appendChild(temperatureEl);
+        card.appendChild(temperatureEl);
         console.log(data.main.temp);
       } else {
         alert("Error 😩");
@@ -173,11 +180,34 @@ function getTokyo() {
     });
 }
 
-//!reset function
-var reset = function () {
-  responseContainerEl.textContent = "";
+var makeCard = function (weather) {
+  console.log(weather.weather);
+  var day = moment(weather.dt_txt).format("dddd");
+  console.log(day);
+  var date = moment(weather.dt_txt).format("MMM Do YY");
+  console.log(date);
+  for (var i = 0; i <= weather.weather.length; i++) {
+    console.log(weather.weather[i]);
+    var ww = weather.weather.description;
+    console.log(ww);
+  }
+
+  //within the loop
+  //create a div for each day
+  //take dt and moment it into day of the week and date
+  //temperature
+  //description
+  //icon
+  //append all into the div
 };
 
-cityEl.addEventListener("#submit-city", getWeather);
+//!reset function
+var reset = function () {
+  //$(responseContainerEl).remove(card);
+  responseContainerEl.innerHTML = "";
+  //id="response-container" class="card-body card hide"
+};
 
-zipCodeEl.addEventListener("#submit-zip", getByZip);
+cityEl.addEventListener("click", getWeather);
+
+zipCodeEl.addEventListener("click", getByZip);
